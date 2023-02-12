e911fff84ef1fedb0528613231df2af94df81a79
Amit Bhandari <ooooo7247@gamil.com>
2023-02-12 13:03:59 +0900

    チュートリアル2/12

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import style from '@/styles/Home.module.css'
-import '@/styles/calender.css'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,15 +21,13 @@ export default function Home() {
       <h1 id="header">2023 1</h1>
 
       <div id="next-last-btn">
-        <button id="last" onClick={last()}></button>
-       <button id="next" onClick={next()}></button>
+        <button id="last" onClick={last}>＜</button>
+       <button id="next" onClick={next}>＞</button>
       </div>
       <div id="calender"></div>
-
+    <table></table>
 
     </div>
-
-    
 
     </>
   )
@@ -38,37 +36,36 @@ export default function Home() {
 
 const week = ["日", "月", "火", "水", "木", "金", "土"];
 const today = new Date();
-// 月末だとずれる可能性があるため、1日固定で取得
-var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
-// 初期表示
-window.onload = function () {
-    showProcess(today, calendar);
-};
-// 前の月表示
+var showDate = new Date(today.getFullYear(), today.getMonth(),1);
+
+
 function last(){
     showDate.setMonth(showDate.getMonth() - 1);
     showProcess(showDate);
+    console.log(showDate)
 }
 
-// 次の月表示
+
 function next(){
     showDate.setMonth(showDate.getMonth() + 1);
     showProcess(showDate);
+    console.log("chip")
 }
+last();
+next();
+//Object.prototype.toString.call("button.next")
 
-// カレンダー表示
 function showProcess(date: Date) {
     var year = date.getFullYear();
     var month = date.getMonth();
-    document.querySelector('#header')?.innerHTML = year + "年 " + (month + 1) + "月";
+    //document.querySelector('#header')?.innerHTML = year + "年 " + (month + 1) + "月";
 
     var calendar = createProcess(year, month);
-    document.querySelector('#calendar').innerHTML = calendar;
+    //document.querySelector('#calendar').innerHTML = calendar;
 }
 
   function createProcess(year: number, month: number) {
-    // 曜日
     var calendar = "<table><tr class='dayOfWeek'>";
     for (var i = 0; i < week.length; i++) {
         calendar += "<th>" + week[i] + "</th>";
@@ -81,20 +78,15 @@ function showProcess(date: Date) {
     var lastMonthEndDate = new Date(year, month, 0).getDate();
     var row = Math.ceil((startDayOfWeek + endDate) / week.length);
 
-    // 1行ずつ設定
     for (var i = 0; i < row; i++) {
-        calendar += "<tr>";
-        // 1colum単位で設定
+        calendar += "<tr>"
         for (var j = 0; j < week.length; j++) {
             if (i == 0 && j < startDayOfWeek) {
-                // 1行目で1日まで先月の日付を設定
                 calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
             } else if (count >= endDate) {
-                // 最終行で最終日以降、翌月の日付を設定
                 count++;
                 calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
             } else {
-                // 当月の日付を曜日に照らし合わせて設定
                 count++;
                 if(year == today.getFullYear()
                   && month == (today.getMonth())
